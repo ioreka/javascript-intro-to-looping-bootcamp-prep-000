59fc0fa841c631e89575716ed9affbc6c0cd3e60
Lucy Mitchell <lucymhodges@gmail.com>
2018-04-26 15:01:53 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,5 +20,11 @@ return "done"
 }
 
 function doWhileLoop(array) {
-  
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+do {
+  console.log('doo-bee-doo-bee-doo')
+} while (maybeTrue());
 }
